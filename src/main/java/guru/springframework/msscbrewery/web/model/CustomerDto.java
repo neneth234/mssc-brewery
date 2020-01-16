38bc47be9d58886de594854bdbef7f9a04f4290d
Jaakko <neneth2@gmail.com>
2020-01-16 21:59:33 +0200

    First assignment completed!

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class CustomerDto {
+
+    private UUID id;
+    private String name;
+
+}
